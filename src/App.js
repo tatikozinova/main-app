aefdb5611da1c9a1bb6d69d7bcb463accce60fff
Tetians Kozinova <tetianskozinova@Tetianss-Air.attlocal.net>
2023-01-24 11:00:58 -0800

    your commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {data} from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[items, setItems] = useState(0);
+  
+  const [showMore, setShowMore]=useState(false);
+  const [showText, setShowText]=useState(false);
+
+  const {id, product, image, description} = data[items];
+
+  const removeItem=(id)=>{
+    let newSkincare=items.filter(item=>item.id !==id);
+    setItems (newSkincare)
+  }
+
+const showTextClick=(item)=>{
+  item.showMore=!item.showMore
+  setShowText(!showText)
+}
+
+const previousItem =()=>{
+setItems ((item=>{
+item --;
+if (item>0){
+  return data.length -1;
+}
+return item;
+}))
+}
+
+const nextItem =()=>{
+  setItems ((item=>{
+    item ++;
+    if (item>data.length -1){
+      item=0;
+    }
+    return item;
+  }))
+}
+
+return(
+  <div>
+    <div className='container'>
+      <h1>{items.length} products of my current skincare</h1>
     </div>
-  );
+  
+
+  <div className='container'>
+    <img src={image} width='300px' alt='skincare'/>
+  </div>
+
+<div className='container'>
+  <h2>{id}. {product}</h2>
+</div>
+
+<div className='container'>
+  <h2>{description}</h2>
+</div>
+
+<div className='btn container'>
+<button onClick ={previousItem}>Previous</button>
+<button onClick={nextItem}>Next</button>
+</div>
+
+<div className='container'>
+  <button onClick={()=>removeItem(id)}>Remove</button>
+</div>
+
+<div className='container'>
+  <button onClick={()=>setItems([])}>Delete all</button>
+</div>
+
+</div>
+)
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const data =[
+    {
+        id: 1,
+        product: 'cream',
+        image: 'https://cdn.shopify.com/s/files/1/0496/2900/3927/products/C-TetraDayCream-Lifestyle-2000x2000.jpg?crop=center&height=581&v=1651001076&width=581',
+        description: `The silky cream formula, with an uplifting citrus scent, provides daily hydration and helps to reduce the appearance of premature skin aging.
+        C-Tetra Day Cream contains tetrahexyldecyl ascorbate, a stabilized form of vitamin C and an antioxidant. Tetrahexyldecyl ascorbate helps to reduce the appearance of fine lines and wrinkles, while visibly brightening skin tone.
+        The formula also harnesses superior hydration power by combining hyaluronic acid and glycerin, working together to leave the complexion visibly supple and moisturized.`
+    },
+    {
+        id: 2,
+        product: 'serum',
+        image: 'https://www.paulaschoice.com/dw/image/v2/BBNX_PRD/on/demandware.static/-/Sites-pc-catalog/default/dw9c06758f/images/products/resist-10-percent-niacinamide-booster-7980-L.png?sw=360&sfrm=png',
+        description: 'Niacinamide is a form of vitamin B3, which research has proven can reduce the appearance of enlarged pores, uneven skin tone, wrinkles, and dry skin. The light-as-water texture is designed to dramatically enhance your skincare while also providing soothing agents to promote visibly calm skin. Antioxidants and skin-replenishing ingredients make this product a multi-tasking must-have solution for all skin types.'
+    },
+    {
+        id: 3,
+        product: 'acid',
+        image:'https://cdn.shopify.com/s/files/1/1331/0021/products/MANDELIC8_MATTEBLACK_web.png?v=1605802810',
+        description: `A dramatically different AHA experience synergistically formulated to target the top three trouble spots—acne, aging, irregular pigmentation—in one simple, unsurpassed serum that’s safe for all skin tones.`
+    },
+    {
+        id: 4,
+        product: 'retinol',
+        image: 'https://cdn.shopify.com/s/files/1/0496/2900/3927/products/20220420_-_Product_Page_-_Crystal_Retinal_Foil_Icon_-_US_-_2000_x_2000_V1.jpg?crop=center&height=2048&v=1651573151&width=1920',
+        description: 'The more stable the molecule, the more effective it can be on the skin. Crystal Retinal offers a level of retinal stability that is only found at Medik8: our innovative stability system features our renowned crystal encapsulation and a patent-pending formula complex. The dual-layered stability system comes together to protect the retinal molecule for unmatched potency and long-lasting effectiveness.'
+    },
+
+    {
+        id: 5,
+        product: 'spf',
+        image: 'https://8c3412d76225d04d7baa-be98b6ea17920953fb931282eff9a681.images.lovelyskin.com/ewhlai33_202007291754150292.jpg',
+        description: `This ultra sheer, broad spectrum SPF50 formulation provides UVA/UVB protection while it's PerfecTint technology enriches natural skin tones. Formulated for daily use and extended outdoor activities, ECLIPSE SPF 50+ quickly absorbs for a non-greasy matte finish.`
+    }
+]
